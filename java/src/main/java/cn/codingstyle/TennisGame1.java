793c75e04a71e1723c57c2e12b4ee02b2717f837
simon <hanzhenfeng@wuxicloud.com>
2021-05-09 20:59:51 +0800

    refactor: get the part score when points < 3

--- java/src/main/java/cn/codingstyle/TennisGame1.java
+++ java/src/main/java/cn/codingstyle/TennisGame1.java
@@ -1,71 +1,63 @@
 package cn.codingstyle;
 
 public class TennisGame1 implements TennisGame {
-    
-    private int player1Score = 0;
-    private int player2Score = 0;
+
+    private int player1Points = 0;
+    private int player2Points = 0;
 
     public void wonPoint(String playerName) {
         if (playerName.equals("player1")) {
-            player1Score += 1;
+            player1Points += 1;
         } else {
-            player2Score += 1;
+            player2Points += 1;
         }
     }
 
     public String getScore() {
-        String score = "";
-        int tempScore=0;
-        if (player1Score == player2Score)
-        {
-            switch (player1Score)
-            {
+        String result = "";
+
+        if (player1Points == player2Points) {
+            switch (player1Points) {
                 case 0:
-                        score = "Love-All";
+                    result = "Love-All";
                     break;
                 case 1:
-                        score = "Fifteen-All";
+                    result = "Fifteen-All";
                     break;
                 case 2:
-                        score = "Thirty-All";
+                    result = "Thirty-All";
                     break;
                 default:
-                        score = "Deuce";
+                    result = "Deuce";
                     break;
-                
+
             }
-        }
-        else if (player1Score >=4 || player2Score >=4)
+        } else if (player1Points >= 4 || player2Points >= 4) // end game
         {
-            int minusResult = player1Score - player2Score;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
+            int minusResult = player1Points - player2Points;
+            if (minusResult == 1) result = "Advantage player1";
+            else if (minusResult == -1) result = "Advantage player2";
+            else if (minusResult >= 2) result = "Win for player1";
+            else result = "Win for player2";
+        } else {
+            result = getScoreName(player1Points) + "-" + getScoreName(player2Points);
         }
-        else
-        {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = player1Score;
-                else { score+="-"; tempScore = player2Score;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
-            }
+        return result;
+    }
+
+    private String getScoreName(int points) {
+        if (points == 0) {
+            return "Love";
         }
-        return score;
+        if (points == 1) {
+            return "Fifteen";
+        }
+        if (points == 2) {
+            return "Thirty";
+        }
+        if (points == 3) {
+            return "Forty";
+        }
+        return "";
     }
 }
